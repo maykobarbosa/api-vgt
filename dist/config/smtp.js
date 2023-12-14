bdecaca514242909957c36aeeaefdba7772cabb9
Mayko Barbosa <barbosa.mayko@gmail.com>
2023-12-14 00:15:01 -0300

    remove

--- dist/config/smtp.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-module.exports = {
-    host: 'smtp.ma.gov.br',
-    port: 587,
-    user: process.env.EMAIL_ADDRESS,
-    pass: process.env.PASS
-};
-//# sourceMappingURL=smtp.js.map
