e5adcc91f0e7f8f211c71fd983b0d0575712bd78
riangon <rian.goncallves@gmail.com>
2025-04-17 22:31:48 -0300

    [fix] checktoken

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -5,9 +5,9 @@ import { checkToken } from "../middlewares/validaToken";
 const userRoutes = Router()
 const userController = new UserController()
 
-userRoutes.post("/autenticar-usuario", checkToken, userController.AutenticarUsuario)
-userRoutes.post("/registrar-empreendedor", checkToken, userController.RegistrarContaEmpreendedor)
-userRoutes.post("/registrar-investidor", checkToken, userController.RegistrarContaInvestidor)
+userRoutes.post("/autenticar-usuario", userController.AutenticarUsuario)
+userRoutes.post("/registrar-empreendedor", userController.RegistrarContaEmpreendedor)
+userRoutes.post("/registrar-investidor", userController.RegistrarContaInvestidor)
 userRoutes.get("/listar-usuario/:usuarioId", checkToken, userController.ListarUsuarioPeloId)
 userRoutes.get("/listar-investidores", checkToken, userController.ListarInvestidores)
 userRoutes.get("/listar-empreendedores", checkToken, userController.ListarEmpreendedores)
