73dc03e4f099a0b8eef5e21e89377c8871c09008
riangon <rian.goncallves@gmail.com>
2025-04-18 10:59:40 -0300

    migration documentos da empresa podem ser null

---
+++ prisma/migrations/20250418135705_documentos_empresa_podem_ser_null/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE `empresas` MODIFY `imposto` VARCHAR(191) NULL,
+    MODIFY `YTD` VARCHAR(191) NULL,
+    MODIFY `despesas` VARCHAR(191) NULL,
+    MODIFY `dividas` VARCHAR(191) NULL,
+    MODIFY `receitas` VARCHAR(191) NULL,
+    MODIFY `contratos_firmados` VARCHAR(191) NULL,
+    MODIFY `contratos_pendentes` VARCHAR(191) NULL;
